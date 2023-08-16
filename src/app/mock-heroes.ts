49b52576ac640d22e40e3a1fe71dc4a54d3fdcdb
Vansh <vanshgarg32@gmail.com>
2023-08-16 17:07:08 +0530

    First commit with first 2 parts

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from "./hero";
+
+export const HEROES : Hero[] = [
+    {id: 2, name: 'Ichigo'},
+    {id: 3, name: 'Goku'},
+    {id: 4, name: 'Vegeta'},
+    {id: 5, name: 'Spider-man'},
+    {id: 6, name: 'Ikkaku'},
+    {id: 7, name: 'Diljit'},
+    {id: 8, name: 'Luffy'},
+    {id: 9, name: 'Zoro'},
+    {id: 10, name: 'Kakarot'}
+];
