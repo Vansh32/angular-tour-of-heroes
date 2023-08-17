5e4c4d1d0e4af1bbdd44fbd88fd35f34cf578d7b
Vansh <vanshgarg32@gmail.com>
2023-08-17 13:37:57 +0530

    Commit with 4 parts complete

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,10 +10,31 @@ import { HEROES } from '../mock-heroes';
 })
 
 export class HeroesComponent {
-  heroes = HEROES;
+  
+  constructor(private heroService: HeroService, private messageService: MessageService) {}
+
   selectedHero?: Hero;
+  heroes: Hero [] = [];
 
   onSelect(hero: Hero): void {
   this.selectedHero = hero;
+  this.messageService.add(`Heroes Component: Select Hero id = ${hero.id}`);
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }  
+
+
+
+  // getHeroes(): void {
+  //   this.heroes = this.heroService.getHeroes();
+  // }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
   }
 }
+
+
